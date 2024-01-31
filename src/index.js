871d1a1a0d524e974543e783279ad732033d7e93
EDM115 <dev@edm115.eu.org>
2024-01-31 10:58:59 +0100

    0.0.0 the bases

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const blinder = require('color-blind');
+import chroma from "chroma-js";
+
+module.exports = {
+	blinder: blinder,
+	chroma: chroma
+};
---
+++ vite.config.mjs
@@ -0,0 +1,39 @@
+import commonjs from 'vite-plugin-commonjs'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    vue(),
+    process.env.ANALYZE && visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      template: 'treemap'
+    }),
+    commonjs({
+      include: ['node_modules/chroma-js/chroma.js']
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3000,
+    hmr: {
+      port: 3000,
+      protocol: 'ws'
+    },
+    publicPath: '/'
+  },
+  build: {
+    commonjsOptions: {
+      include: [/chroma-js/]
+    }
+  }
+})
